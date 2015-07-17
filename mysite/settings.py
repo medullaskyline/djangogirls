67cb738104d0e205074d2db455085ccd5d9813d1
medullaskyline <kelly.e.iverson@gmail.com>
2015-07-16 23:25:55 -0700

    added post_detail view

--- mysite/settings.py
+++ mysite/settings.py
@@ -102,3 +102,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# STATIC_ROOT = ''
+
+# STATIC_URL = '/static/'
+
+# STATICFILES_DIRS = ( os.path.join('static'), )
